317caacea8fb2a41327b42bfe4b60c17ccc8a2ab
RafaelFariaFerreira <rafaelfariaferreira1@gmail.com>
2023-07-25 11:46:13 -0300

    Seeders for contries, states and cities

--- database/migrations/2023_07_24_000004_create_cities_table.php
+++ database/migrations/2023_07_24_000004_create_cities_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->string('code', 2)->unique();
             $table->foreignId('state_id')->constrained('states')->cascadeOnDelete();
 
         });
