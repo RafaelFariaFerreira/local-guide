997e51a981a7db382b8ceedf8905c1e4299454ac
RafaelFariaFerreira <rafaelfariaferreira1@gmail.com>
2023-08-07 18:08:37 -0300

    FrontController

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class FrontController extends Controller
+{
+    public function home()
+    {
+        return Inertia('App/Home');
+    }
+    public function appointments()
+    {
+        return Inertia('App/Appointments');
+    }
+    public function following()
+    {
+        return Inertia('App/Following');
+    }
+    public function purchases()
+    {
+        return Inertia('App/Purchases');
+    }
+    public function profile()
+    {
+        return Inertia('App/Profile');
+    }
+}
