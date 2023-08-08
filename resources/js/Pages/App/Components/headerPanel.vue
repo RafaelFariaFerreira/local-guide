41ff6296bb08a7c6add6ffe78502603b84d34f8d
RafaelFariaFerreira <rafaelfariaferreira1@gmail.com>
2023-08-08 18:25:15 -0300

    headerPanel component refactor

--- resources/js/Pages/App/Components/headerPanel.vue
+++ resources/js/Pages/App/Components/headerPanel.vue
@@ -13,10 +13,9 @@ defineProps({
         class="flex-start flex flex-row items-center justify-start gap-x-3 border-b border-gray-200 px-5 py-5"
     >
         <Icon />
-        <h1 class="text-md flex-grow text-center font-bold text-gray-800">
+        <h1 class="text-md flex-grow text-center font-bold text-gray-800 pr-9">
             {{ title }}
         </h1>
-        <div class="h-6 w-6"></div>
     </header>
 </template>
 
